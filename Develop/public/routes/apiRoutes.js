c7d600c78c58b486eeeaec828b422b048bc8709e
Chris Messa <blindsun95@gmail.com>
2023-05-23 17:28:01 -0500

    delete route completed!

--- Develop/public/routes/apiRoutes.js
+++ Develop/public/routes/apiRoutes.js
@@ -4,43 +4,71 @@ const fs = require('../../helpers/fsUtils')
 const { v4: uuidv4 } = require('uuid');
 
 
-
+// Get list of notes
 apiNotes.get('/api/notes', (req, res) => {
    fs.readFromFile(path.join(__dirname, '../../db/db.json'), 'utf8')
     .then((data) => {
         const db = JSON.parse(data);
         res.json(db);
+        console.log("Got notes!");
+    })
+    .catch(err => {
+        console.error(err)
+        res.statusCode(404).json({ message: "Sorry, the notes are missing!" });
     })
 })
 
 apiNotes.post('/api/notes', (req, res) => {
+
+    /*
+    *   Functions the same as: 
+    *   const title = req.body.title;
+    *   const text = req.body.text;
+    */
+    
     const { title, text } = req.body;
     if (title && text) {
         const newNote = { title, text, id: uuidv4() };
         fs.readFromFile(path.join(__dirname, '../../db/db.json'))
         .then((data) => {
             let db = JSON.parse(data);
-            if (Array.isArray(db)) {
-                db.push(newNote);
-              } else {
-                console.error('Data in db.json is not an array');
-                res.status(500).json({ message: "Internal server error" });
-                return; 
-              }
-            fs.writeToFile(
-                path.join(__dirname, '../../db/db.json'),
-                db
-            );
+            db.push(newNote);
+            fs.writeToFile(path.join(__dirname, '../../db/db.json'), db);
             res.status(201).json(db);
         })
         .catch((err) => {
             console.error(err);
-            res.status(500).json({ message: "An error occurred while saving the note" });
+            res.status(500).json({ message: "Note not saved" });
         });
     } else {
-        res.status(400).json({ message: "Note needs a title and text" });
+        res.status(400).json({ message: "Note needs title and text" });
     }
 });
+
+apiNotes.delete('/api/notes/:id', (req, res) => {
+    const id = req.params.id; 
+    if (id) {
+        fs.readFromFile(path.join(__dirname, '../../db/db.json'))
+        .then((data) => {
+            let db = JSON.parse(data);
+
+            const noteIndex = db.findIndex(note => note.id === id);
+
+            if (noteIndex === -1) {
+                res.statusCode(404).json({ message: "No note to delete"})
+            } else {
+            db.splice(noteIndex, 1);
+
+            fs.writeToFile(path.join(__dirname, '../../db/db.json'), db);
+            res.json({ message: "Note Deleted" })
+            };
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({ message: "Note could not be deleted properly"})
+        })
+    }
+})
 
 module.exports = apiNotes;
 
