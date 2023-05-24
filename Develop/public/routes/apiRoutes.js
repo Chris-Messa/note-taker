2ca2c7cd427e414b01a8796c293dfbac570b41fd
Chris Messa <blindsun95@gmail.com>
2023-05-23 20:29:13 -0500

    Revert "adding comments"
    This reverts commit 2872c6f166230f0ba3f3a593bfe5dd2a90962726.

--- Develop/public/routes/apiRoutes.js
+++ Develop/public/routes/apiRoutes.js
@@ -25,24 +25,14 @@ apiNotes.post('/api/notes', (req, res) => {
     *   const title = req.body.title;
     *   const text = req.body.text;
     */
-
+    
     const { title, text } = req.body;
-
-    // Check if title and text exist in req.body (that is, determines if they are truthy values)
     if (title && text) {
-
-        // Defines a new note
         const newNote = { title, text, id: uuidv4() };
-
-        // accesses the data in db.json, interpreted as a string
         fs.readFromFile(path.join(__dirname, '../../db/db.json'))
         .then((data) => {
-
-            // assigns the mutable variable db to the data returned from the readFromFile operation and parses it into a JSON object
             let db = JSON.parse(data);
             db.push(newNote);
-
-            // Replaces the existing db.json file with the new JSON array after pushing the new note
             fs.writeToFile(path.join(__dirname, '../../db/db.json'), db);
             res.status(201).json(db);
         })
@@ -55,29 +45,20 @@ apiNotes.post('/api/notes', (req, res) => {
     }
 });
 
-// Sets route for deleting a note based on its unique ID
 apiNotes.delete('/api/notes/:id', (req, res) => {
-
-    // assigns the constant variable id to the value of id paramater from the route URL id
     const id = req.params.id; 
     if (id) {
-
-
         fs.readFromFile(path.join(__dirname, '../../db/db.json'))
         .then((data) => {
             let db = JSON.parse(data);
 
-            // Assigns the constant variable noteIndex to the index of the note with the matching requested id in the db array 
             const noteIndex = db.findIndex(note => note.id === id);
 
-            // Checks if there are no notes in the array
             if (noteIndex === -1) {
                 res.statusCode(404).json({ message: "No note to delete"})
             } else {
-            // if there are notes, remove the note at the specified index to be deleted
             db.splice(noteIndex, 1);
 
-            // replace the existing file with the new file that has the note deleted from the array
             fs.writeToFile(path.join(__dirname, '../../db/db.json'), db);
             res.json({ message: "Note Deleted" })
             };
--- Develop/public/routes/htmlRoutes.js
+++ Develop/public/routes/htmlRoutes.js
@@ -1,13 +1,10 @@
 const htmlNotes = require('express').Router();
 const path = require('path');
-
-// Route for root page get request
 htmlNotes.get('/', (req, res) => {
     console.log('user got the resource');
     res.sendFile(path.join(__dirname, '../index.html'))
 })
 
-// Route for the notes page get request
 htmlNotes.get('/notes', (req, res) => {
     console.log('notes page!');
     res.sendFile(path.join(__dirname, '../notes.html'));
