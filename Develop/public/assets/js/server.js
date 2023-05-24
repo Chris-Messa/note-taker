1fac515699bb4ad29017aae43faf4da4b0d08149
Chris Messa <blindsun95@gmail.com>
2023-05-23 20:22:52 -0500

    don't know what I'm doing

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -2,12 +2,16 @@ const express = require('express');
 const api = require('../../routes/apiRoutes.js')
 const html = require('../../routes/htmlRoutes')
 const PORT = process.env.PORT || 3001;
+const path = require('path');
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static('public'))
+app.use(express.static('public');
+
+console.log('__dirname', __dirname);
+console.log('static files being served from:', path.join(__dirname, './public'))
 
 app.use('/', api)
 app.use('/', html)
